a84fd87f371bc0aed9a71ce8b7eda5bc5de89a4a
meltest <meltest@example.com>
2022-07-14 11:32:48 +0900

    improve search option

--- search.sh
+++ search.sh
@@ -2,31 +2,51 @@
 
 CMDNAME=`basename $0`
 TIMESTAMP=`TZ=UTC-9 date "+%Y%m%d%H%M%S"`
+TZ=UTC
 
-while getopts sha:p:r: OPT
+ADDRESS=""
+PORT=""
+STATUS=""
+
+while getopts ha:p:s: OPT
 do
-        case $OPT in
-                "a" ) AVALUE="$OPTARG"; AGGRESSION="-a $AVALUE"; AFLAG="${AVALUE}_" ;;
-                "p" ) PVALUE="$OPTARG"; PROXY="-p $PVALUE" ;;
-                "h" ) echo "Usage: $CMDNAME [-a ipaddress] [-p port] json_file" 1>&2
-                        exit 1 ;;
-                * ) echo "Usage: $CMDNAME [-a ipaddress] [-p port] json_file" 1>&2
-                        exit 1 ;;
-        esac
+    case $OPT in
+        "a" )
+            ADDRESS='.value.ip=="'$OPTARG'"'
+            ;;
+        "p" )
+            PORT=".value.port==$OPTARG"
+            ;;
+        "s" )
+            STATUS='.value.status=="'$OPTARG'"'
+            ;;
+        "h" ) echo "Usage: $CMDNAME [-a ipaddress] [-p port] json_file" 1>&2
+                exit 1 ;;
+        * ) echo "Usage: $CMDNAME [-a ipaddress] [-p port] json_file" 1>&2
+                exit 1 ;;
+    esac
 done
 
-TZ=UTC
+options=($ADDRESS $PORT $STATUS)
+is_first=1
+query=""
+
+for option in ${options[@]}
+do
+    if [[ ! -z $option  && $is_first -eq 1 ]]; then
+        query+="${option}"
+        is_first=0
+    elif [[ ! -z $option && $is_first -eq 0 ]]; then
+        query+=" and ${option}"
+    fi
+done
 
 shift `expr $OPTIND - 1`
 
-if [[ -z $AVALUE && -z $PVALUE ]]; then
-  cat $1 | jq -c '._default | to_entries | map(.key, .value)' | perl -pe 's/},{/}\n{/g'
-elif [[ ! -z $AVALUE && -z $PVALUE ]]; then
-  cat $1 | jq -c '._default | to_entries | map(select(.value.ip == "'$AVALUE'")) | map(.key, .value)' | perl -pe 's/},/}\n{/g'
-elif [[ -z $AVALUE && ! -z $PVALUE ]]; then
-  cat $1 | jq -c '._default | to_entries | map(select(.value.port == '$PVALUE')) | map(.key, .value)' | perl -pe 's/},/}\n{/g'
-elif [[ ! -z $AVALUE && ! -z $PVALUE ]]; then
-  cat $1 | jq -c '._default | to_entries | map(select(.value.ip == "'$AVALUE'" and .value.port == '$PVALUE')) | map(.key, .value)' | perl -pe 's/},/}\n{/g'
+if [[ -z $query  ]]; then
+    echo "cat $1 | jq -c '._default | to_entries | map(.key, .value)' | perl -pe 's/},{/}\n{/g'"
+    cat $1 | jq -c '._default | to_entries | map(.key, .value)' | perl -pe 's/},{/}\n{/g'
 else
-  cat $1 | jq -c '._default | to_entries | map(.key, .value)' | perl -pe 's/},{/}\n{/g'
+    echo "cat $1 | jq -c '._default | to_entries | map(select(${query})) | map(.key, .value)' | perl -pe 's/},/}\n{/g'"
+    cat $1 | jq -c "._default | to_entries | map(select(${query})) | map(.key, .value)" | perl -pe 's/},/}\n{/g'
 fi
